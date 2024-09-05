d2a8b1dac4fbb2c1809de55a105c576206fca94e
Ivarock <ivarssluka@gmail.com>
2024-09-05 13:26:08 +0300

    Refactor validation for 'store' and 'update' methods

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCVRequest;
+use App\Http\Requests\UpdateCVRequest;
 use App\Models\CV;
 use App\Services\PDFGeneratorService;
 use Dompdf\Dompdf;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-/**
- * @method authorize(string $string, CV|Collection|Model|null $cv)
- */
 class CVController extends Controller
 {
     use AuthorizesRequests;
@@ -43,36 +39,16 @@ class CVController extends Controller
         return view('cv.create');
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(StoreCVRequest $request): RedirectResponse
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:15',
-            'email' => 'required|email|max:255',
-            'address' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'linkedin' => 'nullable|url',
-            'github' => 'nullable|url',
-            'education.*.institution' => 'required|string|max:255',
-            'education.*.degree' => 'required|string|max:255',
-            'education.*.start_date' => 'required|date',
-            'education.*.end_date' => 'nullable|date',
-            'work_experience.*.company' => 'required|string|max:255',
-            'work_experience.*.position' => 'required|string|max:255',
-            'work_experience.*.start_date' => 'required|date',
-            'work_experience.*.end_date' => 'nullable|date',
-            'work_experience.*.description' => 'nullable|string',
-        ]);
-
         $cv = CV::create(['user_id' => Auth::id()]);
 
         $cv->personalDetails()->create($request->only([
             'name', 'surname', 'phone_number', 'email', 'address', 'description', 'linkedin', 'github'
         ]));
 
-        foreach ($request->education as $edu) {
-            $cv->education()->create($edu);
+        foreach ($request->education as $education) {
+            $cv->education()->create($education);
         }
 
         foreach ($request->work_experience as $work) {
@@ -91,31 +67,11 @@ class CVController extends Controller
         return view('cv.edit', compact('cv'));
     }
 
-    public function update(Request $request, $id): RedirectResponse
+    public function update(UpdateCVRequest $request, $id): RedirectResponse
     {
         $cv = CV::findOrFail($id);
 
         $this->authorize('update', $cv);
-
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:15',
-            'email' => 'required|email|max:255',
-            'address' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'linkedin' => 'nullable|url',
-            'github' => 'nullable|url',
-            'education.*.institution' => 'required|string|max:255',
-            'education.*.degree' => 'required|string|max:255',
-            'education.*.start_date' => 'required|date',
-            'education.*.end_date' => 'nullable|date',
-            'work_experience.*.company' => 'required|string|max:255',
-            'work_experience.*.position' => 'required|string|max:255',
-            'work_experience.*.start_date' => 'required|date',
-            'work_experience.*.end_date' => 'nullable|date',
-            'work_experience.*.description' => 'nullable|string',
-        ]);
 
         $cv->personalDetails->update($request->only([
             'name', 'surname', 'phone_number', 'email', 'address', 'description', 'linkedin', 'github'
